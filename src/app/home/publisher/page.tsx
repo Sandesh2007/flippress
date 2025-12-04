08f67afaab21a41d8e45812fa7926a324e1f7e97
SAN_DESH63 <99701679+Sandesh2007@users.noreply.github.com>
2025-12-04 13:15:23 +0545

    init: initial V2

--- src/app/home/publisher/page.tsx
+++
@@ -1,5 +0,0 @@
-import Dashboard from "./components/DashboardMain"
-
-export default function PublisherPage() {
-  return <Dashboard />
-}
--- src/app/protected/page.tsx
+++
@@ -1,9 +0,0 @@
-import UserProfile from '@/app/profile/page'
-
-const Page = () => {
-  return (
-    <UserProfile/>
-  )
-}
-
-export default Page
